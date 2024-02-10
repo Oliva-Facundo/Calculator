aa3d5bc5ec9eafcefb6fc3fe998e86d7fde82944
Kairobisky <facundooliva500@hotmail.com>
2024-02-10 15:52:48 -0300

    Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
       <div
         className={`h-screen w-screen font-leagueSpartan flex items-center justify-center bg-bgMain text-[32px] text-mainText`}
       >
-        <main className={`w-[32rem] h-[45rem]`}>
+        <main className={`max-w-[32rem] max-h-[45rem] w-[32rem] max-sm:min-w-[20rem] max-sm:p-4`}>
           <div className="flex justify-between items-end mb-8">
             <h1 className="text-xl leading-7 tracking-tight ml-2">calc</h1>
             <div className="flex items-end gap-[1.625rem] text-sm leading-3">
@@ -71,7 +71,7 @@ function App() {
                   ))}
                 </div>
                 <div
-                  className="w-[72px] h-[26px] bg-bgToggle rounded-full flex justify-between cursor-pointer *:appearance-none *:cursor-pointer relative before:size-4 before:bg-bgTertiary before:hover:bg-bgTertiaryHover before:rounded-full before:absolute before:left-[5px] before:top-[5px] before:transition-all before:translate-x-[var(--offset)]"
+                  className="w-[72px] h-[26px] bg-bgToggle rounded-full flex justify-between cursor-pointer *:appearance-none *:cursor-pointer relative before:size-4 before:bg-bgTertiary before:hover:bg-bgTertiaryHover before:rounded-full before:absolute before:left-[5px] before:top-[5px] before:transition-all before:translate-x-[var(--offset)]" 
                   style={switchStyle}
                 >
                   {themes.map((theme, index) => (
@@ -89,11 +89,11 @@ function App() {
             </div>
           </div>
           <div
-            className={`screen h-[8rem] rounded-lg bg-bgScreen mb-6 flex items-center justify-end pr-8 text-6xl overflow-x-auto whitespace-nowrap`}
+            className={`h-[8rem] rounded-lg bg-bgScreen mb-6 flex items-center justify-end pr-8 text-6xl overflow-x-auto whitespace-nowrap max-w-full `}
           >
             {nums}
           </div>
-          <div className="grid grid-cols-4 gap-6 p-8 rounded-lg bg-bgToggle w-full text-primaryText">
+          <div className="grid grid-cols-4 gap-6 p-8 rounded-lg bg-bgToggle w-full text-primaryText max-sm:gap-4">
             <button
               className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons("7")}
@@ -101,103 +101,103 @@ function App() {
               7
             </button>
             <button
-              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover"
+              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons("8")}
             >
               8
             </button>
             <button
-              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover"
+              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons("9")}
             >
               9
             </button>
             <button
-              className="h-14 py-2 uppercase rounded-lg bg-bgSecondary shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-secondaryShadow text-mainText hover:bg-bgSecondaryHover"
+              className="h-14 py-2 uppercase rounded-lg bg-bgSecondary shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-secondaryShadow text-mainText hover:bg-bgSecondaryHover text-2xl active:translate-y-1"
               onClick={deleteNum}
             >
               del
             </button>
             <button
-              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover"
+              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons("4")}
             >
               4
             </button>
             <button
-              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover"
+              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons("5")}
             >
               5
             </button>
             <button
-              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover"
+              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons("6")}
             >
               6
             </button>
             <button
-              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover"
+              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons("+")}
             >
               +
             </button>
             <button
-              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover"
+              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons("1")}
             >
               1
             </button>
             <button
-              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover"
+              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons("2")}
             >
               2
             </button>
             <button
-              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover"
+              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons("3")}
             >
               3
             </button>
             <button
-              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover"
+              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons("-")}
             >
               -
             </button>
             <button
-              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover"
+              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons(".")}
             >
               .
             </button>
             <button
-              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover"
+              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons("0")}
             >
               0
             </button>
             <button
-              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover"
+              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons("/")}
             >
               /
             </button>
             <button
-              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover"
+              className="h-14 py-2 rounded-lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-primaryShadow bg-bgPrimary hover:bg-bgPrimaryHover active:translate-y-1"
               onClick={() => handleButtons("*")}
             >
               x
             </button>
             <button
-              className={`h-14 py-2 rounded-lg col-span-2 uppercase bg-bgSecondary shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-secondaryShadow text-mainText hover:bg-bgSecondaryHover`}
+              className={`h-14 py-2 rounded-lg col-span-2 uppercase bg-bgSecondary shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-secondaryShadow text-mainText hover:bg-bgSecondaryHover text-2xl active:translate-y-1`}
               onClick={clear}
             >
               reset
             </button>
             <button
-              className={`h-14 py-2 rounded-lg col-span-2 bg-bgTertiary hover:bg-bgTertiaryHover lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-tertiaryShadow text-mainText`}
+              className={`h-14 py-2 rounded-lg col-span-2 bg-bgTertiary hover:bg-bgTertiaryHover lg shadow-[0_4px_0_rgba(0,0,0,0.5)] shadow-tertiaryShadow text-mainText active:translate-y-1`}
               onClick={result}
             >
               =
